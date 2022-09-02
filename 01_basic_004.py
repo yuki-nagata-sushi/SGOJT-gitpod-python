a4cf4b120f7f9e24c543ed163b722b4753f448d5
yuki-nagata-sushi <yuki-nagata@kke.co.jp>
2022-09-02 02:31:41 +0000

    add request_code

---
+++ 01_basic_004.py
@@ -0,0 +1,11 @@
+import requests
+
+r = requests.request(method="GET", url="https://www.python.jp")
+#r2 = requests.get(url="https://www.python.jp") こんな書き方もあるよ
+
+print(r) #これだとステータス200が返る事しかわからない
+print(r.text) #こうするとちゃんとHTMLが見える
+
+with open("r.txt", mode="w") as f:
+    f.write(r.text) #txtに書き出しても、日本語の文字化けは直らない。terminalの問題ではなさそう。
+
